e6654c28a644b90c741d40c5e985cc2220f45c1a
Luke Zeches <zechesl@gmail.com>
2024-03-13 15:02:52 -0400

    Set up storage

---
+++ LocalStorage.js
@@ -0,0 +1,12 @@
+import Storage from "react-native-storage";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+// Documentation: https://github.com/sunnylqm/react-native-storage
+const storage = new Storage({
+  storageBackend: AsyncStorage,
+  defaultExpires: null, // Entries will not expire unless a specific expiry time is specified during save
+  // Write async methods that run if the code tries to access a key or key-id in storage that does not exist
+  sync: {},
+});
+
+export default storage;
