3f81a86667a8a7a091baf8d87b89f0d25686e339
Luke Zeches <zechesl@gmail.com>
2024-04-15 13:02:16 -0400

    Scaffold AES encryption

---
+++ encryption.js
@@ -0,0 +1,18 @@
+/*
+https://www.splunk.com/en_us/blog/learn/data-encryption-methods-types.html
+Select points of encryption/decryption and which data needs to be encrypted/decrypted
+Identify methods of encrytion
+For each method:
+- Describe how it works
+- Implement it in the system
+- Analyze security vs. complexity
+
+Encryption methods
+Symmetric: quicker, best suited to larger datasets, smaller cyphertexts
+Advanced Encryption Standard (AES) - most frequently used
+Blowfish (TDES) - less secure than AES
+
+Asymmetric: slower, best suited to larger datasets, larger cyphertexts - public and private key
+Rivest Shamir Adleman (used by HTTPS, so used by Firebase)
+Elliptic Curve Cryptography (ECC)
+*/
